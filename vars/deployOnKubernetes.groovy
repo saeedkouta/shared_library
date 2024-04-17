b38ef3dc8eb571779584db31dc93f14968ce7af7
AliKhamed <46407148+AliKhamed@users.noreply.github.com>
2024-04-18 01:01:14 +0200

    Update deployOnKubernetes.groovy

--- vars/deployOnKubernetes.groovy
+++ vars/deployOnKubernetes.groovy
@@ -2,9 +2,6 @@
 
 // KubernetesCredentialsID 'KubeConfig file'
 def call(String k8sCredentialsID, String imageName) {
-    
-    // Update deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
     // login to k8s Cluster via KubeConfig file
     withCredentials([file(credentialsId: "${k8sCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
